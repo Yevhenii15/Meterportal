7f3a443db1f8faea6fe069e2c34a61b5c049bec4
Yevhenii <yevmot01@easv365.dk>
2025-09-15 15:35:10 +0200

    frontend fix

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <!-- If you want a navbar/footer on ALL pages, you can put them here -->
+  <NuxtPage />
+  <!-- This is where your route's page gets rendered -->
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </form>
+  </div>
+</template>
